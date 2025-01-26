83817a831c111d3010075aa0112110759b732dc1
VictorTimbo <manoelvictortimbo@gmail.com>
2025-01-26 16:45:40 -0300

    create service layer, component registration

--- src/main/java/com/victortimbo/course/resources/UserResource.java
+++ src/main/java/com/victortimbo/course/resources/UserResource.java
@@ -1,18 +1,30 @@
 package com.victortimbo.course.resources;
 
 import com.victortimbo.course.entities.User;
+import com.victortimbo.course.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> getUser() {
-        User user = new User(1L, "maria", "maria@gmail.com", "12345", "789456123");
+    public ResponseEntity<List<User>> getUser() {
+        List<User> users = service.findAll();
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
 }
